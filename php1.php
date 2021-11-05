764ae22c9f75cae88c213657696e41758e5bcc42
Supakran48 <69299126+Supakran48@users.noreply.github.com>
2021-11-05 11:42:38 +0700

    Add files via upload

---
+++ php1.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta chaeset="utf8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP: Basic</title>
+        <script>
+            h1{
+                color:red;
+            }
+    </head>
+
+    <body>
+        <h1 style="color:blue;">
+        <?php echo "PHP Syntax"; ?>
+        </h1>
+    </body>
+</html>
